4f42250cd8eefb1f7f7adb3b15705bc467f6aad7
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-01-27 09:37:56 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -18,8 +18,7 @@ from pyrogram.types import (
 
 from pyrogram.types.messages_and_media import Message
 from vars import API_HASH, API_ID, BOT_TOKEN, WEBHOOK, PORT
-from utils import getstatusoutput, is_youtube_link, remove_chars, remove_duplicate_line, replace_space, is_valid_link
-    
+from utils import progress_bar
 from aiohttp import web
 
 
